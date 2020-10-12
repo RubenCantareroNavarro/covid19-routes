b5c97e71520eb35e0f3b27ad14a20d89cb2a172d
Ruben.Cantarero <Ruben.Cantarero@uclm.es>
2020-10-12 13:48:21 +0200

    Soporte lenght validacion

--- src/utilities/routes_operations.py
+++ src/utilities/routes_operations.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8; mode: python3; -*-
-
-
-def compare_routes(route1, route2, danger_points_data):
-    route1_danger_points = number_danger_points_in_route(danger_points_data, route1)
-    route2_danger_points = number_danger_points_in_route(danger_points_data, route2)
-
-    return [route1_danger_points, route2_danger_points]
-
+import osmnx as ox
 
 def number_danger_points_in_route(danger_points_data, route):
     cont = 0
     danger_points = []
+    last_point = []
+
     for feature in danger_points_data['features']:
         danger_points.append(feature['geometry']['coordinates'])
 
     for point in route['geometry']['coordinates']: 
+        if last_point == point:
+            continue
+
         if(point in danger_points):
             cont += 1
 
+        last_point = point
+
     return cont
+
+def route_cost(G, route):
+    return sum([G[route[i]][route[i+1]][0]['length'] for i in range(len(route)-1)])
+
+def coordinates_to_graph_nodes(G, route):
+    route_nodes = []
+    last_point = []
+
+    for point in route['geometry']['coordinates']:
+        #If possible that the user make the mistake putiing the same point in the visualizer
+        if last_point == point: 
+            continue
+
+        route_nodes.append(ox.get_nearest_node(G, [point[1], point[0]]))
+        last_point = point
+
+    return route_nodes
+
+    # for i in range(0, len(route['geometry']['coordinates']) - 1, 2):
+    #     point1 = route['geometry']['coordinates'][i]
+    #     point2 = route['geometry']['coordinates'][i + 1]
+
+    #     node1 = ox.get_nearest_node(G, [point1[1], point1[0]])
+    #     node2 = ox.get_nearest_node(G, [point2[1], point2[0]])
+
+    #     route_edges.append(G.adj[node1][node2][0]["osmid"])
+
+    # print(route_edges)
+    # return route_edges
+    
--- src/validation_survey.py
+++ src/validation_survey.py
@@ -7,6 +7,7 @@ import networkx as nx
 import utilities
 import os
 import json
+ox.config(use_cache=True, log_console=False)
 
 # Define and prepare data
 bottom_left = [38.954487, -3.958351]
@@ -32,7 +33,11 @@ def calculate_best_routes():
         destination_node = ox.get_nearest_node(G, [destiny[1], destiny[0]])
 
         route = nx.shortest_path(G, origin_node, destination_node, weight='length')
-        best_routes[case_id] = utilities.route_to_geojson(G, route)
+        best_routes[case_id] = {}
+        best_routes[case_id]["route_nodes"] = route
+        best_routes[case_id]["route_coordinates"] = utilities.route_to_geojson(G, route)
+        best_routes[case_id]["route_length"] = utilities.routes_operations.route_cost(G, route)
+        best_routes[case_id]["number_danger_points"] = utilities.routes_operations.number_danger_points_in_route(danger_points_data, best_routes[case_id]["route_coordinates"])
     
     return best_routes
 
@@ -40,20 +45,24 @@ if __name__ == '__main__':
     with open(amenities_file) as geojson_file:
         amenities = json.load(geojson_file)
 
+    with open(danger_nodes_file_cache) as json_file:
+        danger_points_data = json.load(json_file)
+
     G = utilities.init_graph(bottom_left, top_right, graph_file_cache, amenities)
     utilities.load_danger_points(bottom_left, top_right, danger_nodes_file_cache, amenities)
 
     best_routes = calculate_best_routes()
-    
-    with open(danger_nodes_file_cache) as json_file:
-        danger_points_data = json.load(json_file)
 
+ 
     for currentpath, folders, files in os.walk(survey_dir):
         for file in files:            
             with open(os.path.join(survey_dir, currentpath, file)) as json_file:
                 route = json.load(json_file)
-
                 case_id = route['properties']['case_id']
-                result = utilities.routes_operations.compare_routes(best_routes[case_id], route, danger_points_data)
 
-                print(result)
+                survey_number_danger_points = utilities.routes_operations.number_danger_points_in_route(danger_points_data, route)
+                survey_route_nodes = utilities.routes_operations.coordinates_to_graph_nodes(G, route)
+                survey_route_length = utilities.routes_operations.route_cost(G, survey_route_nodes)
+
+                print([file, case_id, [survey_number_danger_points, survey_route_length], [best_routes[case_id]["number_danger_points"], best_routes[case_id]["route_length"]]])
+
