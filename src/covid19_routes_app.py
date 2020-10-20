5240c49ec260d51981df811e428f9e00a200c607
Ruben.Cantarero <Ruben.Cantarero@uclm.es>
2020-10-20 15:14:48 +0200

    Changed survey routes files

--- src/covid19_routes_app.py
+++ src/covid19_routes_app.py
@@ -24,7 +24,7 @@ bottom_left = [38.954487, -3.958351]
 top_right = [39.012350, -3.863268]
 graph_file_cache = os.path.join(project_dir, 'src/cache/ciudad-real-graph.graphml')
 danger_nodes_file_cache = os.path.join(project_dir, 'src/cache/ciudad-real-danger-nodes.geojson')
-survey_directory = os.path.join(project_dir, "data/survey/")
+survey_directory = os.path.join(project_dir, "~/covid19-routes-data/survey/")
 amenities_file = os.path.join(project_dir, 'config/amenities_config.json')
 survey_config_file = os.path.join(project_dir, 'config/validation_survey_cases.json')
 
